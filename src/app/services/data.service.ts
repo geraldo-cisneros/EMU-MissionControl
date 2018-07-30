ed73b6c8e51f6e7aaf829ef930275c8751a369ce
geraldo-cisneros <geraldo-cisneros@users.noreply.github.com>
2018-07-30 14:46:22 -0500

    fixed console errors

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { catchError } from '../../../node_modules/rxjs/operators';
+import { throwError, Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { HttpResponse } from '../../../node_modules/@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   title = 'app works!';
-  private apiUrl= 'http://localhost:3000';
+  private apiUrl= 'http://localhost:3000/';
   data: any = {};
 
-  constructor(public http: Http) { 
+  constructor(private http: Http) { 
     console.log('Data service connected');
-    this.getContacts();
-    this.getData();
   }
-  
+
 
   getData(){
     var x = "test";
      return this.http.get(this.apiUrl) 
-     .map((res: Response) => res.json());
+     .pipe(map(this.extractData),
+       catchError( this.handleErrorObservable)
+  );
+  }
+
+  private extractData(res: Response)
+  {
+    let body = res.json();
+    return body;
+  }
+
+  private handleErrorObservable (error: Response | any)
+  {
+    return throwError(error.message || error);
   }
 
   getContacts() {
