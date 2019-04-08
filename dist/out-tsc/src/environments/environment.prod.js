2f3951a3e8b8204f0c84c317895b2c690edee0b2
geraldo-cisneros <41762712+geraldo-cisneros@users.noreply.github.com>
2019-04-08 14:37:10 -0500

    ud

--- dist/out-tsc/src/environments/environment.prod.js
+++ dist/out-tsc/src/environments/environment.prod.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = {
     production: true,
     //URL of development API
-    apiURL: 'http://localhost:3000'
+
 };
 //# sourceMappingURL=environment.prod.js.map
